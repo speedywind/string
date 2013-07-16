3f1dcf000651ae917c8ee0bd22712f863a7167f6
xinji <messagenote@qq.com>
2013-07-16 17:23:24 +0800

    7/16

--- test.cpp
+++ test.cpp
@@ -1,1 +1,42 @@
-hello world
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int getLength(int number)
+{
+    cout<<"1";
+    int length = 0;
+    while(number!=0)
+    {
+        number/=10;
+        length++;
+    }
+    //cout<<"length:"<<length;   
+    return length;
+}
+int main()
+{
+    char* abc = "1999asd-23s5432131d11asd123123";
+    char *pp = abc;
+    int lartemp = 0;  //largest temp
+    int temp=0;
+cout<<"input :"<<abc<<endl;
+    while(*pp++!='\0')
+    {
+	if(*pp >='0' && *pp<='9' ){
+	    temp=atoi(pp);
+	    if (lartemp<temp) {
+
+		lartemp=temp;
+		cout<<"lartemp changed :"<<temp<<endl;
+		}
+    		pp+=getLength(temp)-1;
+	}
+	else if(*pp=='-');
+	else
+	cout<<*pp;
+    }
+    std::cout<<lartemp;
+    return 0;
+
+}
